e04ab50fbf0f1a52b78a4cd1c0dd849296087a9d
FranGozze <fgozzerino@gmail.com>
2020-08-23 18:21:49 -0300

    Me olvide de guardar en el anterior commit

--- float.s
+++ float.s
@@ -12,14 +12,18 @@ main:       li $v0, 4
             syscall 
             move $t0, $f0               #Mueve el float a t0
 
-            andi $t1, $t0, 7F000000     #le hago and al nro para sacar el exponente(01111111000000000000000000000000)
+            andi $t1, $t0, 7F800000     #le hago and al nro para sacar el exponente(01111111100000000000000000000000)
 
             andi $t2, $t0, 00FFFFFF     #le hago and al nro para sacar la mantiza(00000000111111111111111111111111)
 
-            sra $t1, $t1, 24
+            srl $t1, $t1, 23            #Desplazo el exponente 23 lugares a la derecha
+            andi $t3, $t1, 80           #hago un and para ver el signo del exponente y posteriormente moverlo
+            sll $t3, $t3, 24            #Desplazo el signo del exponente al final del nro
+            andi $t1, $t1, 7F           #Elimino el signo
+            or $t1, $t1, $t3
 
             li $v0,4
-            la $a0, Mantiza
+            la $a0, Mantiza         
             syscall
             
             move $a0, $t2
