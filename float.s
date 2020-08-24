bfd40440804eab3ac28245b6d40f3e73a9ec1ef2
Alan <alan@example.com>
2020-08-24 08:45:24 -0300

    Notacion hexadecimal 0x, globl main, cambio move en registro flotante.

--- float.s
+++ float.s
@@ -1,37 +1,45 @@
-        .data
-Ingrese:   .asciiz "Ingrese el numero real: " 
-Mantiza:   .asciiz "La mantiza es: "
-Expo:   .asciiz "El exponente es: "
+# Segmento de datos
+.data
+    Ingrese:     .asciiz "Ingrese el numero real: " 
+    Mantisa:     .asciiz "La mantisa es: "
+    Expo:        .asciiz "El exponente es: "
+    nuevaLinea:  .asciiz "\n"
 
-    .text #zona de instrucciones
-main:       li $v0, 4
+# Segmento de codigo 
+.text
+.globl main
+main:
+            li $v0, 4
             la $a0, Ingrese
             syscall
             
-            li $v0,6                    #Lee el float y lo pone en f0
+            li $v0, 6                   # Lee el float y lo pone en f0
             syscall 
-            move $t0, $f0               #Mueve el float a t0
+            mfc1 $t0, $f0
+  
+            andi $t1, $t0, 0x7F800000   # Hago and al nro para sacar el exponente (01111111100000000000000000000000)
 
-            andi $t1, $t0, 7F800000     #le hago and al nro para sacar el exponente(01111111100000000000000000000000)
+            andi $t2, $t0, 0x00FFFFFF   # Hago and al nro para sacar la mantisa (00000000111111111111111111111111)
 
-            andi $t2, $t0, 00FFFFFF     #le hago and al nro para sacar la mantiza(00000000111111111111111111111111)
-
-            srl $t1, $t1, 23            #Desplazo el exponente 23 lugares a la derecha
-            andi $t3, $t1, 80           #hago un and para ver el signo del exponente y posteriormente moverlo
-            sll $t3, $t3, 24            #Desplazo el signo del exponente al final del nro
-            andi $t1, $t1, 7F           #Elimino el signo
+            srl $t1, $t1, 23            # Desplazo el exponente 23 lugares a la derecha
+            andi $t3, $t1, 0x80         # Hago un and para ver el signo del exponente y posteriormente moverlo
+            sll $t3, $t3, 24            # Desplazo el signo del exponente al final del nro
+            andi $t1, $t1, 0x7F         # Elimino el signo
             or $t1, $t1, $t3
 
-            li $v0,4
-            la $a0, Mantiza         
+            li $v0, 4
+            la $a0, nuevaLinea
             syscall
-            
+            la $a0, Mantisa         
+            syscall
+
             move $a0, $t2
             li $v0, 1
             syscall              
 
-
             li $v0,4
+            la $a0, nuevaLinea
+            syscall
             la $a0, Expo
             syscall
 
@@ -39,6 +47,20 @@ main:       li $v0, 4
             li $v0, 1
             syscall              
 
-
-            li $v0, 10      #termino el programa
+            li $v0,4
+            la $a0, nuevaLinea
             syscall
+            syscall
+            li $v0, 1
+            move $a0, $t0
+            syscall
+
+
+            # Salida el programa
+            li $v0, 10
+            syscall
+
+
+
+
+
