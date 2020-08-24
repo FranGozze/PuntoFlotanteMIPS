c5d3b270f3edfc93ee25af9462f27870228a327b
Alan <alan@example.com>
2020-08-24 11:43:03 -0300

    Cambio mascaras de and's, uso mfc1 para copiar el valor del registro flotante, agrego referencia de pagina web para chequear

--- float.s
+++ float.s
@@ -1,3 +1,5 @@
+# Puede chequear los resultados en https://www.h-schmidt.net/FloatConverter/IEEE754.html
+#
 # Segmento de datos
 .data
     Ingrese:     .asciiz "Ingrese el numero real: " 
@@ -17,15 +19,14 @@ main:
             syscall 
             mfc1 $t0, $f0
   
-            andi $t1, $t0, 0x7F800000   # Hago and al nro para sacar el exponente (01111111100000000000000000000000)
+            andi $t1, $t0, 0xFF800000   # Hago and al nro para sacar el exponente (01111111100000000000000000000000)
 
-            andi $t2, $t0, 0x00FFFFFF   # Hago and al nro para sacar la mantisa (00000000111111111111111111111111)
+            andi $t2, $t0, 0x7FFFFF     # Hago and al nro para sacar la mantisa (00000000111111111111111111111111)
 
             srl $t1, $t1, 23            # Desplazo el exponente 23 lugares a la derecha
             andi $t3, $t1, 0x80         # Hago un and para ver el signo del exponente y posteriormente moverlo
-            sll $t3, $t3, 24            # Desplazo el signo del exponente al final del nro
-            andi $t1, $t1, 0x7F         # Elimino el signo
-            or $t1, $t1, $t3
+            sll $t3, $t3, 8             # Desplazo el signo del exponente al final del nro
+            andi $t1, $t1, 0xFF         # Elimino el signo
 
             li $v0, 4
             la $a0, nuevaLinea
@@ -33,9 +34,9 @@ main:
             la $a0, Mantisa         
             syscall
 
-            move $a0, $t2
             li $v0, 1
-            syscall              
+            move $a0, $t2
+            syscall
 
             li $v0,4
             la $a0, nuevaLinea
@@ -43,24 +44,13 @@ main:
             la $a0, Expo
             syscall
 
+            li $v0, 1
             move $a0, $t1
-            li $v0, 1
-            syscall              
-
-            li $v0,4
-            la $a0, nuevaLinea
-            syscall
-            syscall
-            li $v0, 1
-            move $a0, $t0
             syscall
 
 
             # Salida el programa
             li $v0, 10
             syscall
-
-
-
 
 
